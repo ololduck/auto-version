e28252e892edafc3aa0bcc5bd78824c12d6e262a
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-11-11 15:49:20 +0100

    Dunno, lol

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     url='https://github.com/paulollivier/auto_versionning',
     license='LICENSE.txt',
     description='A simple versionning automation.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
