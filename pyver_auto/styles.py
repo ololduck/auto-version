19e18487ba86bcc089d41faa849aff2c45a06486
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-12-26 18:46:09 +0100

    added base work

---
+++ pyver_auto/styles.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+import json
+import re
+
+
+
+# Full format, aka. :<major>.<minor>.<patch>+<status>-<build>
+class Full:
+
+    FORMAT=r'\d+\.\d+\.\d+\+\w\-\d+'
+
+    def __init__(self):
+        pass
+
+    def parse_file(self, fpath):
+        pass
+
+    def perform_major(self):
+        pass
+
+    def perform_minor(self):
+        pass
+
