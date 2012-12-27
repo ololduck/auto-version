31a7661e536ca2320ce6f72731cbd09d1c1ea35c
Paul Ollivier <paul.ollivier@ullink.com>
2012-12-27 10:00:41 +0100

    Added config file generation

--- pyver_auto/styles.py
+++ pyver_auto/styles.py
@@ -5,8 +5,13 @@ import os
 import json
 import re
 
+SYS_EXIT_CONF_CREATED = 10
+
+
 class ConfFileManager:
-    FNAME="auto_versionning.conf"
+
+    FNAME = "auto_versionning.conf"
+
     def __init__(self):
         if(os.path.exists(self.FNAME)):
             # then read
@@ -14,11 +19,20 @@ class ConfFileManager:
         else:
             # then create
             pass
+            conf = {}
+            conf["last_version"] = None
+            conf["files"] = [{"path":"path/to/file", "format":"full"},
+                            {"path":"path/to/file", "format":"full"}]
+
+            with open(self.FNAME, 'w+') as f:
+                f.write(json.dumps(conf))
+            sys.exit(SYS_EXIT_CONF_CREATED)
+
 
 # Full format, aka. :<major>.<minor>.<patch>+<status>-<build>
 class Full:
 
-    FORMAT=r'\d+\.\d+\.\d+\+\w\-\d+'
+    FORMAT = r'\d+\.\d+\.\d+\+\w\-\d+'
 
     def __init__(self):
         pass
@@ -32,3 +46,8 @@ class Full:
     def perform_minor(self):
         pass
 
+    def perform_patch(self):
+        pass
+
+    def perform_build(self):
+        pass
