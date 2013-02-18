42ec78343b6bfb00ab9db683bc5c7de5940844c4
Paul Ollivier <pollivier@gitlab.local>
2013-02-18 14:13:37 +0100

    Changed name of the module to something less ridiculous

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='pyver_auto',
+    name='auto-version',
     version='0.1.2',
     author='Paul Ollivier  ',
     author_email='contact@paulollivier.fr',
     scripts=['bin/pyver.py'],
     url='https://github.com/paulollivier/auto_versionning',
     license='LICENSE.txt',
+    modules=['auto-version'],
     description='A simple versionning automation.',
     long_description=open('README.md').read(),
 )
