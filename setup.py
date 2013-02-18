9cd96fb508f5c7faa8a25ff8273fd90c9be50284
Paul Ollivier <paul.ollivier.contact@gmail.com>
2013-02-18 23:24:58 +0100

    setup_tools doesn't like markdown

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 from increment_version import __version__
 
+long_description = ""
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='auto-version',
     version=__version__,
@@ -9,7 +14,7 @@ setup(
     scripts=['increment_version.py'],
     url='https://github.com/paulollivier/auto_versionning',
     license='LICENSE.txt',
-    modules=['auto_version'],
+    packages=['auto_version'],
     description='A not-so-simple versionning semi-automation.',
-    long_description=open('README.md', 'r').read(),
+    long_description="TODO: Write a bit more about that"
 )
